a53689aaeece38a3f0e5838e0c07e797200a7034
ArthurBerman <arthurberman94@gmail.com>
2025-01-22 12:01:46 -0500

    Personal site design

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h1>A website for Arthur's projects.</h1>
     <p>
       Do you ever feel like the internet is worse now? I sure do. Arthur's
-      personal site is a return to the good era of the internet. Consider checking out my 
+      personal site is a return to the good era of the internet. Consider checking out my&nbsp;
       <NuxtLink to="/music">music</NuxtLink>
     </p>
   </div>
