a018af8c800b75a456330e5c5dbe58a6efd6eb74
ArthurBerman <arthurberman94@gmail.com>
2025-01-22 11:53:04 -0500

    Add music page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
-<script setup lang="ts">
-const appConfig = useAppConfig();
-</script>
+<template>
+  <div>
+    <h1>A website for Arthur's projects.</h1>
+    <p>
+      Do you ever feel like the internet is worse now? I sure do. Arthur's
+      personal site is a return to the good era of the internet. Consider checking out my 
+      <NuxtLink to="/music">music</NuxtLink>
+    </p>
+  </div>
+  </template>
--- pages/music.vue
+++ pages/music.vue
@@ -1,2 +1,6 @@
-
-    <iframe src="https://youtube.com/embed/Yq1xlH-7Vl4?si=x3ZAouoYpNwq46ZA&autoplay=1" />
+<template>
+    <div>
+        <h1>Music</h1>
+        <iframe src="https://youtube.com/embed/Yq1xlH-7Vl4?si=x3ZAouoYpNwq46ZA" />
+    </div>
+</template>
