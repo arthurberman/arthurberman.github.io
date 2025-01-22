575baeddb64ba447722b1d814df7e4e763c318f4
ArthurBerman <arthurberman94@gmail.com>
2025-01-22 12:16:04 -0500

    Quote component

---
+++ components/Quote.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(['content', 'author'])
+
+console.log(props.content)
+</script>
+<template>
+    <div class="quote-div">
+        <p class="quote">{{ content }}</p>
+        <p class="author">&dash;{{ author }}</p>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <h1>A website for Arthur's projects.</h1>
-    <p>
-      Do you ever feel like the internet is worse now? I sure do. Arthur's
-      personal site is a return to the good era of the internet. Consider checking out my&nbsp;
-      <NuxtLink to="/music">music</NuxtLink>
-    </p>
-  </div>
-  </template>
+    <div>
+        <h1>A website for Arthur's projects.</h1>
+        <Quote content=" Do you ever feel like the internet is worse now? I sure do.  " author="Arthur Berman"/>
+        <p> Arthur's
+            personal site is a return to the good era of the internet. Consider checking out my&nbsp;
+            <NuxtLink to="/music">music</NuxtLink>
+        </p>
+    </div>
+</template>
