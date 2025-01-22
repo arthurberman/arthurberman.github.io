625efbae674914949effd29702282c653c7e4512
ArthurBerman <arthurberman94@gmail.com>
2025-01-22 12:23:06 -0500

    Fix spaces

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>A website for Arthur's projects.</h1>
-        <Quote content=" Do you ever feel like the internet is worse now? I sure do.  " author="Arthur Berman"/>
+        <Quote content=" Do you ever feel like the internet is worse now? I sure do." author="Arthur Berman"/>
         <p> Arthur's
             personal site is a return to the good era of the internet. Consider checking out my&nbsp;
             <NuxtLink to="/music">music</NuxtLink>
