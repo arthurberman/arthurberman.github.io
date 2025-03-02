e6f7956d8d930d41366f04c9e8dd1843e65ea834
ArthurBerman <arthurberman94@gmail.com>
2025-03-02 17:03:41 -0500

    Really trying to make this blog work

---
+++ components/Post.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(['title', 'content'])
+console.log(props.content)
+</script>
+<template>
+    <h2>
+        {{ props.title }}
+    </h2>
+    <slot></slot>
+    {{ props.content }}
+</template>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
         <h1>Blog</h1>
-        <p>Eventually I will write something here, and it will be thoughtless and frivolous.</p>
+        <OneDayEp />
     </div>
 </template>
