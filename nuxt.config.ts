5c69c4efecace153fa381a26ea0d1d92924646ba
ArthurBerman <arthurberman94@gmail.com>
2025-03-03 14:13:29 -0500

    Add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/image", "@nuxtjs/mdc", "@nuxt/content"],
+  content: {
+    build: {
+      markdown: {
+        toc: {
+          depth: 3, // include h3 headings
+        },
+      },
+    },
+  },
 });
