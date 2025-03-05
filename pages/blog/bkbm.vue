518dac3e0dfad447038c08a87fd1ffd1b2cec61b
ArthurBerman <arthurberman94@gmail.com>
2025-03-05 10:35:35 -0500

    Add blog the bad way

---
+++ pages/blog/bkbm.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const content = `
+# Preparing for a one-day EP
+
+On March 1st 2025, I'll be welcoming a group of musician friends to my home studio to write and record an album (or at least an EP) from scratch. I've explored a few options for how to format the day's work.
+
+## Goals:
+
+Release an album on the day it was written and recorded.
+
+Perform at least one track from the album live.
+
+Pay attention to production and quality.
+
+## Possible Approaches
+
+Individual songwriting: taking 30 minutes to write songs individually or in small groups seemed like a good option at first. I've seen it work before: 30 minutes is a limited amount of time, and an expectation of a complete, performable song constructed in that time tends to produce surprising and impressive results.
+
+Pros:
+
+- everybody writes a song
+- we have a good well of performable songs which aren't too complicated
+
+Cons:
+
+- teaching the songs to other bandmates is a pain
+- individual ownership of songs might lead to hurt feelings.
+
+Group songwriting: quickly generate A and B sections one after the next, then stitch them into songs.
+
+Pros:
+
+- lots of options which everybody can immediately connect with
+- stems, loops, and even whole sections can come together really fast
+
+Cons:
+
+- genericization
+- inflexibility
+- no opportunity for lyrics
+
+Overture: freely improvise, and harvest favorite themes
+
+Pros:
+
+- continuity of artistic vision
+
+Cons:
+
+- improvisation in the moment + remembering for later is an intense congnitive load
+
+So, the format for the day is to simply play some group improvisation games, then write some songs based on the experience!
+
+`
+</script>
+<template>
+    <MDC :value="content" />
+</template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-const { data: posts } = await useAsyncData('blog', () => queryCollection('blog').order("date", "DESC").all())
 </script>
 
 <template>
     <div>
         <h1>Blog</h1>
-        <ul>
-            <li v-for="post in posts" :key="post.id">
-                <NuxtLink :to="post.path">{{ post.title }} : {{ post.date }}</NuxtLink>
+        <ul class="tabular">
+            <li>
+                <NuxtLink to="/blog/bkbm">Preparing to record a one-day EP</NuxtLink>
             </li>
         </ul>
     </div>
