c00a1e991994ea90a5639e3558cf06cc8ff2c3f7
ArthurBerman <arthurberman94@gmail.com>
2025-03-04 21:32:42 -0500

    One more try?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxtjs/mdc", "@nuxt/content"],
+  modules: ["@nuxt/image", "@nuxtjs/mdc", "@nuxt/content", "./routes-list"],
   nitro: {
     prerender: {
       crawlLinks: true,
